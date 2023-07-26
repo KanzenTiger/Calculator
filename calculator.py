5015de00a97587c7f2ef094555564208b7f0fba2
Kanzentiger <grihazolotarev@gmail.com>
2023-07-26 21:06:11 +0300

    new version calculator

--- calculator.py
+++ calculator.py
@@ -1,28 +1,44 @@
-d = str(input("Введите действие, которое хотите совершить из ниже пеоечисленны, \n +, -, /, *"))
-try:#если нет типого исключения
-    a = float(input("Введите число: "))
-    b = float(input("Введите число: "))
-    class Calculator():
-        def __init__(self, a, b):
-            self.alpha = a
-            self.beta = b
-        def calculating_P(self, alpha, beta):
-            """Данная функция выполняет вычисления сложения"""
-            c = alpha + beta
-            return c
-        def calculating_M(self, alpha, beta):
-            """Данная функция выполняет вычисления вычитания"""
-            c = alpha - beta
-            return c
-        def calculating_R(self, alpha, beta):
-            """Данная функция выполняет вычисления деления"""
-            c = alpha / beta
-            return c
-        def calculating_U(self, alpha, beta):
-            """Данная функция выполняет вычисления умножения"""
-            c = alpha * beta
-            return c
-    calculator = Calculator(a, b)
-    #print(calculator.)
-except ValueError:#если есть исключение типовое
-    print("Ошибка, вы ввели слово")
+first_operand = float(input("Введите 1 число: "))
+second_operand = float(input("Введите 2 число: "))
+action = str(input("(+)Сложить (-)Вычесть (/)Разделить (*)Умножить\nВыберите действие: "))
+
+class Calculate():
+    def __init__(self, a, b, action):        
+        if action == '+':
+            result = self.addiction(a, b)
+
+        elif action == '-':
+            result = self.subtraction(a, b)
+
+        elif action in ('/', ':'):
+            result =  self.division(a, b)
+
+        elif action in ('*', 'x', 'X'):
+            result =  self.multiplication(a, b)
+
+        else:
+            raise ValueError('Действие не поддерживается.')
+
+        print('Результат:', int(result) if int(result) == result else result)
+
+    def addiction(self, a, b):
+        """Данная функция выполняет вычисления сложения"""
+        c = a + b
+        return c
+
+    def subtraction(self, a, b):
+        """Данная функция выполняет вычисления вычитания"""
+        c = a - b
+        return c
+
+    def division(self, a, b):
+        """Данная функция выполняет вычисления деления"""
+        c = a / b
+        return c
+
+    def multiplication(self, a, b):
+        """Данная функция выполняет вычисления умножения"""
+        c = a * b
+        return c
+
+Calculate(first_operand, second_operand, action)
